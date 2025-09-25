917a247e0d98360779d6c3083ecb6f74e8937bb1
Charlie Zhang <z_char@qq.com>
2025-09-25 14:15:36 -0400

    Pass 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
 const repeatString = function() {
-
+    if (arguments[1] < 0) {
+        return "ERROR";
+    }
+    let result = "";
+    let repeat = arguments[0];
+    for (let i = arguments[1]; i > 0; --i) {
+        result += repeat;
+    }
+    return result;
 };
 
 // Do not edit below this line
