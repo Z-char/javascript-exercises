179c3bbe714621e1b2ad359e2244b8d761579dc8
Charlie Zhang <z_char@qq.com>
2025-09-25 13:58:13 -0400

    Pass 03_numberChecker

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,9 +1,10 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number === 1000) {
     return true;
-  } else {
-    return false;
+  } else if (number == 10) {
+    return true;
   }
+  return false;
 }
 
 // Do not edit below this line
