de50029e1e92f1a5982156bfdd42b359ef43d151
Charlie Zhang <z_char@qq.com>
2025-09-27 17:12:27 -0400

    Pass 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(...args) {
+  if (args.length !== 2) return "ERROR";
+  if (!Number.isFinite(args[0])) return "ERROR";
+  if (!Number.isFinite(args[1])) return "ERROR";
+  const [x, y] = args;
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	if (args.length !== 2) return "ERROR";
+  if (!Number.isFinite(args[0])) return "ERROR";
+  if (!Number.isFinite(args[1])) return "ERROR";
+  const [x, y] = args;
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  args = args.flat();
+	const len = args.length;
+  const valid = args.filter(x => Number.isFinite(x));
+  if (valid.length !== len) return "ERROR";
+  return valid.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  args = args.flat();
+  const len = args.length;
+  const valid = args.filter(x => Number.isFinite(x));
+  if (valid.length !== len) return "ERROR";
+  return valid.reduce((acc, cur) => acc * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(...args) {  
+	if (args.length !== 2) return "ERROR";
+  if (!Number.isFinite(args[0])) return "ERROR";
+  if (!Number.isFinite(args[1])) return "ERROR";
+  const [x, y] = args;
+  return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(...args) {
+  if (args.length !== 1) return "ERROR";
+  if (!Number.isFinite(args[0])) return "ERROR";
+  const x = args[0];
+  if (x === 0) return 1;
+  return factorial(x - 1) * x;
 };
 
 // Do not edit below this line
