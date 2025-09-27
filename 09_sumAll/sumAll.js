a9096acd2fe10c52b82ca41a07e320e65791105c
Charlie Zhang <z_char@qq.com>
2025-09-26 22:45:33 -0400

    Pass 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    if (args.length === 2 && Number.isInteger(args[0]) && Number.isInteger(args[1])) {
+        let [l, r] = args;
+        if (l > r) {
+            [l, r] = [r, l];
+        }
+        if (l < 0) {
+            return "ERROR";
+        }
+        return (l + r) * (r - l + 1) / 2;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
