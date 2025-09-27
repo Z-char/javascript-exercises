b0f4e7927c0c4fc5dd0dcf555f215689f67080d1
Charlie Zhang <z_char@qq.com>
2025-09-26 22:51:11 -0400

    Pass 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(...args) {
+    if (args.length !== 1 || !Number.isInteger(args[0])) return "ERROR";
+    let x = args[0];
+    if (x % 4 !== 0) {
+        return false;
+    }
+    if (x % 100 === 0) {
+        return ((x % 400) === 0);
+    }
+    return true;
 };
 
 // Do not edit below this line
