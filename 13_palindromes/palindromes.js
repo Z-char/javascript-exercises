e92252f2b58c1ce4433e6aad5fc077285c3dba9c
Charlie Zhang <z_char@qq.com>
2025-09-27 17:44:54 -0400

    Pass 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    if (typeof s !== "string") return "ERROR";
+    const valid = s.toLowerCase().replace(/[^a-z0-9]+/g, "");
+    let i = 0, j = valid.length - 1;
+    while (i < j) {
+        if (valid[i] !== valid[j]) return false;
+        i++, j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
