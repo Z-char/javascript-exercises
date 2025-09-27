72d031312ca83ab582db58e356a97d62a16be04d
Charlie Zhang <z_char@qq.com>
2025-09-27 18:08:34 -0400

    Pass 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const getAge = function(x) {
+    const death = x.yearOfDeath, birth = x.yearOfBirth;
+    const thisYear = new Date().getFullYear();
+    return (death ?? thisYear) - birth;
+}
 
+const findTheOldest = function(peoples) {
+    return peoples.reduce((acc, cur) => {
+        const curage = getAge(cur);
+        const accage = getAge(acc);
+        return curage < accage ? acc : cur;
+    });
 };
 
 // Do not edit below this line
